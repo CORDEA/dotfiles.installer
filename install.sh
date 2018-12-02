7ae4232ad37d94bb71e86270b9d4385ed1f179f9
Yoshihiro Tanaka <contact@cordea.jp>
2018-12-02 14:38:40 +0900

    add install script

---
+++ install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# encoding:utf-8
+#
+# Copyright 2018 Yoshihiro Tanaka
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+# Author: Yoshihiro Tanaka <contact@cordea.jp>
+# date  : 2018-12-02
+
+command -v brew > /dev/null 2>&1 || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install git zsh python findutils fzf
+chsh -s $(command -v zsh)
+
+base_dir="$HOME/Documents/git-cloning/"
+installer_dir="/tmp/dotfiles.installer"
+
+git clone https://github.com/CORDEA/dotfiles.installer.git $installer_dir
+for file in $installer_dir/internal/*.sh; do
+    sh -c "$file $base_dir"
+done
+rm -rf $installer_dir
+
+cd $base_dir
+git clone https://github.com/CORDEA/dotfiles.git
+cd dotfiles
+find "$(pwd)/src/" -type f | xargs -I{} ln -sf {} "$HOME/"
+
+cd $base_dir
+git clone git@github.com:CORDEA/dotfiles.internal.git
+cd dotfiles.internal
+find "$(pwd)/src/" -d 1 | xargs -I{} ln -sf {} "$HOME/"
